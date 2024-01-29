a54330d38df3ef829018fa2c961f92ab5aeb2a5e
Camila Alani <alanicamila124@gmail.com>
2024-01-29 05:06:47 -0300

    agregue validacionespara cada campo del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -13,9 +13,54 @@ function Formulario() {
     e.preventDefault();
 
     setMensajeError("");
-}
 
-
+    if (!nombre || !apellido || !dni || !email) {
+        setCamposCompletos(false);
+        return;
+      }
+  
+      if (!/^\d{8}$/.test(dni)) {
+        setMensajeError("El DNI debe contener exactamente 8 números.");
+        return;
+      }
+  
+      if (/^\d+$/.test(email)) {
+        setMensajeError("El email no puede contener solo números.");
+        return;
+      }
+  
+      if (!/^\S+@\S+\.\S+$/.test(email)) {
+        setMensajeError("Ingrese un email válido.");
+        return;
+      }
+  
+      if (nombre.length < 3 || nombre.length > 30) {
+        setMensajeError("El nombre debe tener entre 3 y 30 caracteres.");
+        return;
+      }
+  
+      if (apellido.length < 3 || apellido.length > 30) {
+        setMensajeError("El apellido debe tener entre 3 y 30 caracteres.");
+        return;
+      }
+  
+      if (dni.length !== 8) {
+        setMensajeError("El DNI debe tener exactamente 8 números.");
+        return;
+      }
+  
+      if (email.length < 3 || email.length > 30) {
+        setMensajeError("El email debe tener entre 3 y 30 caracteres.");
+        return;
+      }
+  
+      alert("Datos enviados");
+      setNombre("");
+      setApellido("");
+      setDni("");
+      setEmail("");
+      setCamposCompletos(true);
+    };
 }
 
 export default Formulario;
