251169e3b98edd608c9947952fa85422b1bbaea1
Camila Alani <alanicamila124@gmail.com>
2024-01-29 05:05:03 -0300

    agregue una funcion que se encarga de la logica del formulario y preventDefault para que no se recargue la pagina antes de tiempo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,6 +8,14 @@ function Formulario() {
   const [email, setEmail] = useState("");
   const [camposCompletos, setCamposCompletos] = useState(true);
   const [mensajeError, setMensajeError] = useState("");
+
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+
+    setMensajeError("");
+}
+
+
 }
 
 export default Formulario;
