ffba7b006e4b5fd44421621ad17af78e60bf2264
Camila Alani <alanicamila124@gmail.com>
2024-01-29 05:04:11 -0300

    agregue el useState para los diferentes campos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,13 @@
+import React, { useState } from "react";
 
 
 function Formulario() {
-
-  return (
-    <div>
-      
-    </div>
-  );
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [dni, setDni] = useState("");
+  const [email, setEmail] = useState("");
+  const [camposCompletos, setCamposCompletos] = useState(true);
+  const [mensajeError, setMensajeError] = useState("");
 }
 
 export default Formulario;
